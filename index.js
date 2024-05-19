5e45aa24d1242dc54139a677df1fad4a57dcbe18
patricia202222222222222 <166654962+patricia202222222222222@users.noreply.github.com>
2024-05-19 20:46:36 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let nombre="silvina";
+let apellido="sanchez";
+console.log(nombre +" "+ apellido);
+
+let numberone=120;
+let numbertwo=20;
+let resultsuma= numberone + numbertwo;
+let resultresta= numberone - numbertwo;
+let resultmulti= numberone * numbertwo;
+let resuldivision= numberone / numbertwo;
+console.log("suma",resultsuma);
+console.log ("resta",resultresta);
+console.log ("multiplicacion",resultmulti);
+console.log ("division",resuldivision);
